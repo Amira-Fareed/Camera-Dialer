5eb5d8735c54bc41c24b687bea6e78b003ad98ba
NourhanMS <nourhan_195@hotmail.com>
2019-05-03 02:04:04 +0200

    added detect car function

--- Backend/card.py
+++ Backend/card.py
@@ -3,6 +3,7 @@ import numpy as np
 import imutils
 import matplotlib.pyplot as plt
 
+
 def auto_canny(image, sigma=0.33):
     # compute the median of the single channel pixel intensities
     v = np.median(image)
@@ -32,7 +33,6 @@ def order_points(pts):
     # return the ordered coordinates
     return rect
 
-
 def four_point_transform(image, pts):
     # obtain a consistent order of the points and unpack them
     # individually
@@ -44,7 +44,7 @@ def four_point_transform(image, pts):
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
     maxWidth = max(int(widthA), int(widthB))
-
+    
     # compute the height of the new image, which will be the
     # maximum distance between the top-right and bottom-right
     # y-coordinates or the top-left and bottom-left y-coordinates
@@ -56,10 +56,46 @@ def four_point_transform(image, pts):
     # (i.e. top-down view) of the image, again specifying points
     # in the top-left, top-right, bottom-right, and bottom-left
     # order
-    dst = np.array([[0, 0], [maxWidth - 1, 0], [maxWidth - 1, maxHeight - 1], [0, maxHeight - 1]], dtype="float32")
-
-    # compute the perspective transform matrix and then apply it
+    dst = np.array([[0, 0],[maxWidth - 1, 0],[maxWidth - 1, maxHeight - 1],[0, maxHeight - 1]], dtype = "float32")
+ 
+   # compute the perspective transform matrix and then apply it
     M = cv2.getPerspectiveTransform(rect, dst)
     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
     # return the warped image
     return warped
+
+def detect_card(image):
+    image_gray= cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_gray = cv2.GaussianBlur(image_gray, (5, 5), 0)
+    edges=auto_canny(image_gray)
+    
+    original_height=image.shape[0]
+    original_width=image.shape[1]
+    
+    square_structure_element=cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+    edges=cv2.dilate(edges,square_structure_element,iterations = 2)
+    
+    edges=cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
+    cv2.rectangle(edges,(0,0),(0+original_width,0+original_height),(0,0,0),1)
+    edges= cv2.cvtColor(edges, cv2.COLOR_BGR2GRAY)
+    
+    cnts = cv2.findContours(edges.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+    
+    # loop over the contours
+    for c in cnts:
+        x,y,w,h = cv2.boundingRect(c)
+        if((w*h)<(0.5*(original_height*original_width))):
+            continue 
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        # if our approximated contour has four points, then we
+        # can assume that we have found our screen
+        if len(approx) == 4:
+            screenCnt = approx
+            warped = four_point_transform(image, screenCnt.reshape(4, 2))
+            return warped
+    return image
+
